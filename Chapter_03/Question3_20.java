d9b12c29f7bfc38119d5ab97ccaad31f077c01ba
Adeyemi Adedapo <adedapo025@gmail.com>
2023-12-19 23:44:42 +0100

    Chapter 3 worked on

---
+++ Chapter_03/Question3_20.java
@@ -0,0 +1,14 @@
+package Chapter_03;
+
+/*
+ * (Science: wind-chill temperature) Programming Exercise 2.17 gives a formula to 
+    compute the wind-chill temperature. The formula is valid for temperatures in the 
+    range between -58°F and 41°F and wind speed greater than or equal to 2. Write 
+    a program that prompts the user to enter a temperature and a wind speed. The 
+    program displays the wind-chill temperature if the input is valid; otherwise, it displays a message indicating whether the temperature and/or wind speed is invalid.
+ */
+public class Question3_20 {
+    public static void main(String[] args) {
+        
+    }
+}
