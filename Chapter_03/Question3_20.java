190d885e408835dcfa1389149cf84aa83100d498
Adeyemi Adedapo <adedapo025@gmail.com>
2024-01-08 18:34:15 +0100

    Continued work on repo.

--- Chapter_03/Question3_20.java
+++ Chapter_03/Question3_20.java
@@ -1,5 +1,7 @@
 package Chapter_03;
 
+import java.util.Scanner;
+
 /*
  * (Science: wind-chill temperature) Programming Exercise 2.17 gives a formula to 
     compute the wind-chill temperature. The formula is valid for temperatures in the 
@@ -9,6 +11,23 @@ package Chapter_03;
  */
 public class Question3_20 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         
+        System.out.print("Enter the temperature in Fahrenheit between -58°F and 41°F: ");
+        double temp = input.nextDouble();
+        if (temp < -58 || temp > 41){
+            System.out.println("Invalid range of temperature!");
+            System.exit(1);
+        }
+        System.out.print("Enter wind speed (>= 2) in miles per hour: ");
+        double speed = input.nextDouble();
+        if (speed < 2){
+            System.out.println("Invalid speed input. Enter speed above 2mph!");
+            System.exit(1);
+        }
+        input.close();
+
+        double windChill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
+        System.out.printf("The wind chill index is " + "%.5f", windChill);
     }
 }
